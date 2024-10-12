92835ee62873aa84f1dafd91d1b652490f6559a3
Fabio Sozzi <fabio.sozzi@gmail.com>
2024-10-12 22:25:01 +0200

    Added PHPStan, Laravel Actions, Validated DTO and created Contacts & Organization Model

---
+++ database/migrations/2024_10_12_221041_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Organization;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->foreignIdFor(Organization::class)->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Actions\Contact\CreateContact;
+use App\Actions\Contact\DeleteContact;
+use App\Actions\Contact\GetAllContacts;
+use App\Actions\Contact\GetContact;
+use App\Actions\Contact\UpdateContact;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->name('api.')->group(function () {
+    // Contacts
+    Route::prefix('contacts')->name('contacts.')->group(function () {
+        Route::get('/', GetAllContacts::class)->name('index');
+        Route::post('/', CreateContact::class)->name('store');
+        Route::get('/{contact}', GetContact::class)->name('show');
+        Route::put('/{contact}', UpdateContact::class)->name('update');
+        Route::delete('/{contact}', DeleteContact::class)->name('destroy');
+    });
 })->middleware('auth:sanctum');
